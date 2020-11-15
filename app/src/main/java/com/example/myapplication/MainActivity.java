44b17439bd064857df3527fc5fb0f40854542007
kkkapuq <44130863+kkkapuq@users.noreply.github.com>
2020-11-15 12:32:33 +0900

    종합정보 결과출력 완료 나머지 작업 진행중 - 임시백업

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -133,12 +133,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+
+import Connection.NetworkStatus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.jetbrains.annotations.Nullable;
@@ -146,7 +147,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -159,9 +159,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.ProgressDialog;
-import android.widget.ListAdapter;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public final class MainActivity extends AppCompatActivity {
 
@@ -173,8 +171,9 @@ public final class MainActivity extends AppCompatActivity {
     ArrayList<HashMap<String, String>> timeAnalysis = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, String>> keywordRank = new ArrayList<HashMap<String, String>>();
 
-
     String mJsonString;
+    //결과값 넘겨줄 Intent 선언
+    Intent resultIntent;
 
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -183,13 +182,23 @@ public final class MainActivity extends AppCompatActivity {
         Intent intent = new Intent((Context) this, LoadingActivity.class);
         this.startActivity(intent);
 
+        //결과화면으로 넘겨줄 Intent 생성
+        resultIntent = new Intent(MainActivity.this.getApplicationContext(), ResultActivity.class);
+
         get_Url = (EditText)findViewById(R.id.edt_Url);
         Button btn_search = findViewById(R.id.btn_search);
 
         btn_search.setOnClickListener((new OnClickListener() {
             public final void onClick(View it) {
-                //  Intent intent = new Intent(MainActivity.this.getApplicationContext(), ResultActivity.class);
 
+                //인터넷 연결 체크
+                int status = NetworkStatus.getConnectivityStatus(getApplicationContext());
+                if(status == NetworkStatus.TYPE_MOBILE){
+                }else if (status == NetworkStatus.TYPE_WIFI){
+                }else {
+                    Toast.makeText(getApplicationContext(),"인터넷을 먼저 연결해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 if(get_Url.getText() != null)
                 {
@@ -201,21 +210,20 @@ public final class MainActivity extends AppCompatActivity {
                     //POSTAsyncTask POSTAsyncTask = new POSTAsyncTask();
                     //POSTAsyncTask.execute(baseUrl);
 
-                    //GETAsyncTask GETAsyncTask = new GETAsyncTask();
-                    //GETAsyncTask.execute(baseUrl);
+                    GETAsyncTask GETAsyncTask = new GETAsyncTask();
+                    GETAsyncTask.execute(baseUrl);
 
                     IMGAsyncTask IMGAsyncTask = new IMGAsyncTask();
                     IMGAsyncTask.execute(baseUrl);
 
                     //2.데이터 분석 AsyncTask(TimeSleep)
                     try{
-                        Thread.sleep(5000);
+                        Thread.sleep(10000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     //3.분석 DB조회 및 데이터 가져오기 결과페이지 출력
-
-                    //startActivity(intent);
+//                    startActivity(resultIntent);
                 }
                 else{
                     //비정상적인 링크력
@@ -263,15 +271,11 @@ public final class MainActivity extends AppCompatActivity {
                 outputStream.flush();
                 outputStream.close();
 
-
                 InputStream is = null;
                 BufferedReader in = null;
                 String data = "";
 
-
-
                 int response = httpURLConnection.getResponseCode();
-
 
                 is = httpURLConnection.getInputStream();
                 in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
@@ -283,7 +287,6 @@ public final class MainActivity extends AppCompatActivity {
                 }
                 data = buff.toString().trim();
 
-
                 if (response == HttpURLConnection.HTTP_OK) {
                     Log.e("RECV DATA",data);
                 }
@@ -292,10 +295,7 @@ public final class MainActivity extends AppCompatActivity {
                 }
 
                 in.close();
-
-
                 mJsonString = data;
-
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -315,6 +315,8 @@ public final class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             JsonParsing();
+
+            startActivity(resultIntent);
         }
 
 
@@ -375,12 +377,8 @@ public final class MainActivity extends AppCompatActivity {
                 else{
                     Log.e("NOT RECV DATA",data);
                 }
-
                 in.close();
-
-
                 mJsonString = data;
-
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -393,7 +391,6 @@ public final class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public class IMGAsyncTask extends AsyncTask<String, Void, String>{
 
         @Override
@@ -403,19 +400,16 @@ public final class MainActivity extends AppCompatActivity {
             String getUrl = get_Url.getText().toString();
             String[] DATA = getUrl.split("&");
             String imgName = "";
-            for(int i = 1; i<DATA.length; i++)
-            {
-                Log.e("TEST", DATA[i]);
-                String[] tmp = DATA[i].split("=");
-
-                if(tmp[0].equals("sid1") || tmp[0].equals("oid") || tmp[0].equals("aid"))
-                {
-                    Log.e("TMP0",tmp[0]);
-                    Log.e("TMP1",tmp[1]);
-                    imgName += tmp[1];
-                }
-            }
-            imgName = imgName + ".png";
+//            for(int i = 1; i<DATA.length; i++)
+//            {
+//                String[] tmp = DATA[i].split("=");
+//
+//                if(tmp[0].equals("sid1") || tmp[0].equals("oid") || tmp[0].equals("aid"))
+//                {
+//                    imgName += tmp[1];
+//                }
+//            }
+            imgName = "0010230003574803" + ".png";
             String parameter = "img/";
             url = url + parameter + imgName;
 
@@ -437,7 +431,7 @@ public final class MainActivity extends AppCompatActivity {
                 InputStream is = httpURLConnection.getInputStream();
                 bitmap = BitmapFactory.decodeStream(is);
 
-
+                resultIntent.putExtra("wordcloud", url);
 
 
             } catch (MalformedURLException e) {
@@ -449,7 +443,6 @@ public final class MainActivity extends AppCompatActivity {
             return null;
         }
     }
-
 
 
     private void JsonParsing(){
@@ -505,11 +498,9 @@ public final class MainActivity extends AppCompatActivity {
                 hashMap.put("overSixty", overSixty);
 
                 //Log.e("PUT","FINISH");
-
-
                 tnews.add(hashMap);
-
             }
+            resultIntent.putExtra("tnews", tnews);
             Log.e("ARRAYLIST","tnews FINISH");
 
             for(int i = 0; i<relevanteArticleJarray.length();i++)
@@ -537,6 +528,7 @@ public final class MainActivity extends AppCompatActivity {
                 relevantArticle.add(hashMap);
 
             }
+            resultIntent.putExtra("relevantArticle", relevantArticle);
             Log.e("ARRAYLIST","relevantArticle FINISH");
             for(int i = 0; i<timeAnalysisJarray.length();i++)
             {
@@ -556,10 +548,9 @@ public final class MainActivity extends AppCompatActivity {
                 hashMap.put("count", count);
 
                 //Log.e("PUT","FINISH");
-
                 timeAnalysis.add(hashMap);
-
             }
+            resultIntent.putExtra("timeAnalysis", timeAnalysis);
             Log.e("ARRAYLIST","timeAnalysis FINISH");
             for(int i = 0; i<keywordRankJarray.length();i++)
             {
@@ -582,10 +573,9 @@ public final class MainActivity extends AppCompatActivity {
                 hashMap.put("count", count);
 
                 //Log.e("PUT","FINISH");
-
                 keywordRank.add(hashMap);
-
             }
+            resultIntent.putExtra("keywordRank", keywordRank);
             Log.e("ARRAYLIST","keywordRank FINISH");
             /*
             ListAdapter adapter = new SimpleAdapter(
